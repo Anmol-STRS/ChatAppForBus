17c4ab42ae33129f60fda176c67f5d0149307cfd
Anmol-STRS <anmoldhiman444@gmail.com>
2024-04-16 21:54:54 -0400

    Refactored

--- chatapp/lib/services/notifications.dart
+++ chatapp/lib/services/notifications.dart
@@ -1,258 +1,37 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:flutter/material.dart';
-import 'package:timezone/data/latest.dart' as tz;
-import 'package:timezone/timezone.dart' as tz;
-
-/*
-class LocalNotificationService {
-  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-      FlutterLocalNotificationsPlugin();
-
-  Future<void> init() async {
-    const AndroidInitializationSettings initializationSettingsAndroid =
-        AndroidInitializationSettings('@mipmap/ic_launcher');
-
-    final InitializationSettings initializationSettings =
-        InitializationSettings(android: initializationSettingsAndroid);
-
-    await flutterLocalNotificationsPlugin.initialize(initializationSettings);
-  }
-
-  void showNotificationAndroid(String title, String value) async {
-    const AndroidNotificationDetails androidNotificationDetails =
-        AndroidNotificationDetails(
-      'channel_id',
-      'Channel Name',
-      channelDescription: 'Channel Description',
-      importance: Importance.max,
-      priority: Priority.high,
-      ticker: 'ticker',
-    );
-
-    int notificationId =
-        DateTime.now().millisecondsSinceEpoch ~/ 1000; // Unique ID
-    const NotificationDetails notificationDetails =
-        NotificationDetails(android: androidNotificationDetails);
-
-    await flutterLocalNotificationsPlugin.show(
-      notificationId,
-      title,
-      value,
-      notificationDetails,
-      payload: value, // Use the value as payload
-    );
-
-    void showNotificationAndroid(String title, String value) async {
-      const AndroidNotificationDetails androidNotificationDetails =
-          AndroidNotificationDetails('channel_id', 'Channel Name',
-              channelDescription: 'Channel Description',
-              importance: Importance.max,
-              priority: Priority.high,
-              ticker: 'ticker');
-
-      int notification_id = 1;
-      
-      const NotificationDetails notificationDetails =
-          NotificationDetails(android: androidNotificationDetails);
-
-      await flutterLocalNotificationsPlugin.show(
-          notification_id, title, value, notificationDetails,
-          payload: value);
-    }
-  }
-}
-*/
-
 
 class NotificationService {
-  // Singleton pattern
-  static final NotificationService _notificationService =
-      NotificationService._internal();
-  factory NotificationService() {
-    return _notificationService;
-  }
-
-  NotificationService._internal();
-
-  static const channelId = "1";
-
-  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+  final FlutterLocalNotificationsPlugin notificationsPlugin =
       FlutterLocalNotificationsPlugin();
 
-  static final AndroidNotificationDetails _androidNotificationDetails =
-    const   AndroidNotificationDetails(
-    channelId,
-    "FlutterApp",
-    channelDescription:
-        "This channel is responsible for all the local notifications",
-    playSound: true,
-    priority: Priority.high,
-    importance: Importance.high,
-  );
+  Future<void> initNotification() async {
+    AndroidInitializationSettings initializationSettingsAndroid =
+        const AndroidInitializationSettings('@mipmap/ic_launcher');
 
+    var initializationSettingsIOS = DarwinInitializationSettings(
+        requestAlertPermission: true,
+        requestBadgePermission: true,
+        requestSoundPermission: true,
+        onDidReceiveLocalNotification:
+            (int id, String? title, String? body, String? payload) async {});
 
-  final NotificationDetails notificationDetails = NotificationDetails(
-    android: _androidNotificationDetails,
-  );
-
-  Future<void> init() async {
-    final AndroidInitializationSettings androidInitializationSettings =
-        AndroidInitializationSettings("icon");
-
-
-    final InitializationSettings initializationSettings =
-        InitializationSettings(
-      android: androidInitializationSettings,
-    );
-
-    tz.initializeTimeZones();
-
-    await flutterLocalNotificationsPlugin.initialize(
-      initializationSettings
-    );
+    var initializationSettings = InitializationSettings(
+        android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
+    await notificationsPlugin.initialize(initializationSettings,
+        onDidReceiveNotificationResponse:
+            (NotificationResponse notificationResponse) async {});
   }
 
-  Future<void> requestIOSPermissions() async {
-    await flutterLocalNotificationsPlugin
-        .resolvePlatformSpecificImplementation<
-            IOSFlutterLocalNotificationsPlugin>()
-        ?.requestPermissions(
-          alert: true,
-          badge: true,
-          sound: true,
-        );
+  notificationDetails() {
+    return const NotificationDetails(
+        android: AndroidNotificationDetails('0', 'Anmol',
+            importance: Importance.max),
+        iOS: DarwinNotificationDetails());
   }
 
-  Future<void> showNotification(
-      int id, String title, String body, String payload) async {
-    await flutterLocalNotificationsPlugin.show(
-      id,
-      title,
-      body,
-      notificationDetails,
-      payload: payload,
-    );
-  }
-
-  Future<void> scheduleNotification(int id, String title, String body,
-      DateTime eventDate, TimeOfDay eventTime, String payload,
-      [DateTimeComponents? dateTimeComponents]) async {
-    final scheduledTime = eventDate.add(Duration(
-      hours: eventTime.hour,
-      minutes: eventTime.minute,
-    ));
-    await flutterLocalNotificationsPlugin.zonedSchedule(
-      id,
-      title,
-      body,
-      tz.TZDateTime.from(scheduledTime, tz.local),
-      notificationDetails,
-      uiLocalNotificationDateInterpretation:
-          UILocalNotificationDateInterpretation.absoluteTime,
-      androidAllowWhileIdle: true,
-      payload: payload,
-      matchDateTimeComponents: dateTimeComponents,
-    );
-  }
-
-  Future<void> cancelNotification(int id) async {
-    await flutterLocalNotificationsPlugin.cancel(id);
-  }
-
-  Future<void> cancelAllNotifications() async {
-    await flutterLocalNotificationsPlugin.cancelAll();
+  Future showNotification(
+      {int id = 0, String? title, String? body, String? payLoad}) async {
+    return notificationsPlugin.show(
+        id, title, body, await notificationDetails());
   }
 }
-
-
-/*
-class NotificationService {
-  // Singleton pattern
-  static final NotificationService _notificationService =
-      NotificationService._internal();
-  factory NotificationService() {
-    return _notificationService;
-  }
-
-  NotificationService._internal();
-
-  static const channelId = "1";
-
-  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-      FlutterLocalNotificationsPlugin();
-
-  static final AndroidNotificationDetails _androidNotificationDetails =
-      AndroidNotificationDetails(
-    channelId,
-    "thecodexhub",
-    channelDescription:
-        "This channel is responsible for all the local notifications",
-    playSound: true,
-    priority: Priority.high,
-    importance: Importance.high,
-  );
-
-  final NotificationDetails notificationDetails = NotificationDetails(
-    android: _androidNotificationDetails,
-  );
-
-  Future<void> init() async {
-    final AndroidInitializationSettings androidInitializationSettings =
-        AndroidInitializationSettings('icon');
-
-    final InitializationSettings initializationSettings =
-        InitializationSettings(android: androidInitializationSettings);
-
-    // Initialize timezone
-    tz.initializeTimeZones();
-
-    await flutterLocalNotificationsPlugin.initialize(
-      initializationSettings,
-    );
-  }
-
-  Future<void> showNotification(
-      int id, String title, String body, String payload) async {
-    await flutterLocalNotificationsPlugin.show(
-      id,
-      title,
-      body,
-      notificationDetails,
-      payload: payload,
-    );
-  }
-
-  Future<void> scheduleNotification(int id, String title, String body,
-      DateTime eventDate, TimeOfDay eventTime, String payload,
-      [DateTimeComponents? dateTimeComponents]) async {
-    final scheduledTime = tz.TZDateTime(
-      tz.local,
-      eventDate.year,
-      eventDate.month,
-      eventDate.day,
-      eventTime.hour,
-      eventTime.minute,
-    );
-    await flutterLocalNotificationsPlugin.zonedSchedule(
-      id,
-      title,
-      body,
-      scheduledTime,
-      notificationDetails,
-      uiLocalNotificationDateInterpretation:
-          UILocalNotificationDateInterpretation.absoluteTime,
-      androidAllowWhileIdle: true,
-      payload: payload,
-      matchDateTimeComponents: dateTimeComponents,
-    );
-  }
-
-  Future<void> cancelNotification(int id) async {
-    await flutterLocalNotificationsPlugin.cancel(id);
-  }
-
-  Future<void> cancelAllNotifications() async {
-    await flutterLocalNotificationsPlugin.cancelAll();
-  }
-}
-*/
--- chatapp/lib/theme/theme.dart
+++ chatapp/lib/theme/theme.dart
@@ -1,3 +1,6 @@
+import 'package:chatapp/pages/loginandsignup/loginscreen.dart';
+import 'package:chatapp/pages/loginandsignup/signupscreen.dart';
+import 'package:chatapp/pages/spalshscreen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
@@ -39,22 +42,62 @@ class CustomGradientContainer extends StatelessWidget {
   }
 }
 
-class BottomBar extends StatefulWidget {
-  const BottomBar({super.key});
+
+
+class MyBottomNavBar extends StatefulWidget {
+  const MyBottomNavBar({super.key});
 
   @override
-  State<BottomBar> createState() => _BottomBarState();
+  State<MyBottomNavBar> createState() => _MyButtomNavBarState();
 }
 
-class _BottomBarState extends State<BottomBar> {
+class _MyButtomNavBarState extends State<MyBottomNavBar> {
+  int myCurrentIndex = 0;
+  List pages = const [
+    MainLogoScreen(),
+    SignupScreen(),
+  ];
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      bottomNavigationBar: 
-      GNav(tabs:[
-        GButton(icon: Icons.home)
-      ] 
+    return Scaffold(
+      extendBody: true,
+      bottomNavigationBar: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        decoration: BoxDecoration(boxShadow: [
+          BoxShadow(
+              color: Colors.black.withOpacity(0.5),
+              blurRadius: 25,
+              offset: const Offset(8, 20))
+        ]),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(30),
+          child: BottomNavigationBar(
+              // backgroundColor: Colors.transparent,
+              selectedItemColor: Colors.redAccent,
+              unselectedItemColor: Colors.black,
+              currentIndex: myCurrentIndex,
+              onTap: (index) {
+                setState(() {
+                  myCurrentIndex = index;
+                });
+              },
+              items: const [
+                BottomNavigationBarItem(icon: Icon(Icons.login), label: "Home"),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.favorite), label: "Favorite"),
+                //BottomNavigationBarItem(
+                  //  icon: Icon(Icons.settings), label: "Setting"),
+                //BottomNavigationBarItem(
+                  //  icon: Icon(Icons.person_outline), label: "Profile"),
+              ]),
+        ),
       ),
+      body: pages[myCurrentIndex],
     );
   }
 }
+
+MaterialStateColor customColor = MaterialStateColor.resolveWith((Set<MaterialState> states) {
+  return const Color(0xFFF931AF);
+});
+
